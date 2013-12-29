1d10c9b4537790867047f779e5a340eb2aba37fd
superphil0 <philippomenitsch@gmx.at>
2013-12-29 19:19:22 +0100

    fixed shuffling bug added dokucmentation

--- Kings/www/js/cards.js
+++ Kings/www/js/cards.js
@@ -1,13 +1,16 @@
 cards = [];
 names = ["2","3","4","5","6","7","8","9","10","J","Q","K","A"];
 types = ["spades", "clubs", "diamonds", "hearts"];
+
+// creates a new full card deck
 function createCardDeck()
 {
+	cards = [];
 	for( i = 0; i<52; i++)
 	{
 		name = names[i%13];
 		color = i <26 ? "black" : "red";
-		type = types[Math.round(i/13)]
+		type = types[Math.floor(i/13)]
 		cards.push({id: i, name: name, color: color, type: type});
 	}
 	
@@ -18,7 +21,7 @@ function createCardDeck()
 function shuffle()
 {
 // To shuffle an array a of n elements (indices 0..n-1):
-  for (i= 0; i>0; i--)
+  for (i= cards.length-1; i>0; i--)
   {
 	j = Math.floor(Math.random() * (i+1));
 	tmp = cards[j];
@@ -26,5 +29,8 @@ function shuffle()
 	cards[i] = tmp;
   }
 }
-
-
+// return undefined if there are no cards left
+function getNextCard()
+{
+	return cards.pop();
+}
--- Kings/www/js/index.js
+++ Kings/www/js/index.js
@@ -16,7 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-cards = require("cards.js");
+
 var app = {
     // Application Constructor
     initialize: function() {
